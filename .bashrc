6ee790a62e9e47f46da4adb4692fb3aa89c7841d
Alari Õis <alari.ois.ao@gmail.com>
2024-01-26 16:32:31 +0200

    feat(nvim): use custom shell in vim commands

--- .bashrc
+++ .bashrc
@@ -232,7 +232,6 @@ PERL_LOCAL_LIB_ROOT="/home/alari/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_R
 PERL_MB_OPT="--install_base \"/home/alari/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=/home/alari/perl5"; export PERL_MM_OPT;
 
-source ~/scripts/cbor/.cbor
 . "$HOME/.cargo/env"
 
 # THIS MESSES UP NVIM REAL BAD!
--- vim-shell.sh
+++ vim-shell.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# Log the received command
+# echo "Received command: $@" >> ~/vim-shell-debug.log
+
 # Source the aliases file
 if [ -f ~/.aliases ]; then
+    # echo "Sourcing ~/.aliases" >> ~/vim-shell-debug.log
     source ~/.aliases
 fi
 
-# Execute the command passed to the script
-"$@"
-
+# Execute the command
+if [ "$1" = "-c" ]; then
+    shift  # Remove the first argument ("-c")
+    eval "$@" || echo "Command failed: $@" >&2
+else
+    "$@" || echo "Command failed: $@" >&2
+fi
