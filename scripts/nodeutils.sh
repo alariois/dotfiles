9ad1585996f5ed70e59c67520a6e9e646483b749
Alari Õis <alari.ois.ao@gmail.com>
2024-08-26 15:38:29 +0300

    feat: nodeutils

--- scripts/nodeutils.sh
+++ scripts/nodeutils.sh
@@ -2,8 +2,10 @@
 npmrn() {
   # Split package name and version by '@'
   local input=$1
+
+  # Find the last '@' symbol in the input
   local package=${input%@*}
-  local version=${input#*@}
+  local version=${input##*@}
 
   # If no version is specified, assume @latest
   if [[ "$version" == "$package" || "$version" == "" ]]; then
@@ -18,3 +20,10 @@ npmrn() {
   find -not -path '*node_modules*' -name 'package.json' | xargs sed -i "s#\(\"$package\" *: *\"[\^~]*\)[0-9]\+\.[0-9]\+\.[0-9]\+\"#\1$version\"#"
 }
 
+# just updates all the packages to the latest
+pnpmupdate() {
+  for package in $(pnpm outdated -r --json | jq -r 'keys[]'); do npmrn "$package@latest"; done
+}
+
+export -f npmrn
+export -f pnpmupdate
